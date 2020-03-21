b513f0e056213dd5134bc2740d1f4c35691a3b7d
Robin Gareus <robin@gareus.org>
2020-03-21 03:47:58 +0100

    NO-OP: remove unused #includes

--- libs/ardour/pan_controllable.cc
+++ libs/ardour/pan_controllable.cc
@@ -17,7 +17,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "ardour/pannable.h"
+#include "ardour/pan_controls.h"
 #include "ardour/panner.h"
 #include "ardour/pan_controllable.h"
 
